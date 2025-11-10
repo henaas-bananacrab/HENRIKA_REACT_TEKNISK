4f215b64f330c5d99a367f4bb0c675102fb03815
henaas-bananacrab <henrikaas2008@gmail.com>
2025-11-10 13:26:41 +0100

    har legget til en mappe som hetter Components hvor jeg har laget en komponent som heter Count

--- HENRIKA_REACT_TEKNISK/src/App.jsx
+++ HENRIKA_REACT_TEKNISK/src/App.jsx
@@ -1,9 +1,11 @@
+import Count from './Components/Count'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <Count/>
     </>
   )
 }
---
+++ HENRIKA_REACT_TEKNISK/src/Components/Count.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function Count() {
+    const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Count
